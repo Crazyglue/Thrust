d442cf5d6b54043593ca130d44cef1bf1bcbcaa9
Crazyglue <edbz2k2@gmail.com>
2016-10-07 10:29:09 -0500

    Better sync offline logging

--- app/stores/sync_offline.js
+++ app/stores/sync_offline.js
@@ -3,7 +3,7 @@
 import offline from 'react-native-simple-store';
 
 export default function(store) {
-  console.log("Creating store...");
+  console.log("Creating syncOffline store...");
   let syncedUsername;
   let syncedPassword;
 
@@ -11,16 +11,21 @@ export default function(store) {
 
   store.subscribe(() => {
     const state = store.getState();
-    console.log("Syncing offline...");
+    if(state.login.password || state.login.username || state.movie.recentResult != recentResult)
+      console.log("Syncing offline...");
 
-    console.log("Syncing username: " + state.login.username);
-    console.log(state.login.username);
+    if(state.login.username) {
+      console.log("Syncing username: " + state.login.username);
+      console.log(state.login.username);
+      offline.save("username", state.login.username);
+    }
 
-    console.log("Syncing password: " + state.login.password);
-    console.log(state.login.password);
+    if(state.login.password) {
+      console.log("Syncing password: " + state.login.password);
+      console.log(state.login.password);
+      offline.save("password", state.login.password);
+    }
 
-    offline.save("username", state.login.username);
-    offline.save("password", state.login.password);
 
     if(state.movie.recentResult != recentResult) {
       console.log("Syncing recent result:");
