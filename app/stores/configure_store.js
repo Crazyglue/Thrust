fd00c07aed01f95a95810d58fc3ce69b4bd53cf1
Crazyglue <edbz2k2@gmail.com>
2016-10-16 13:34:48 -0500

    Add redux devtools and wire up to react-native-debugger

--- app/stores/configure_store.js
+++ app/stores/configure_store.js
@@ -1,17 +1,28 @@
 /*jshint esversion: 6 */
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from '../reducers';
 import syncOffline from './sync_offline';
 import promiseMiddleware from 'redux-promise-middleware';
+import { composeWithDevTools } from 'remote-redux-devtools';
 
 export default function configureStore(initialState) {
+  const enhancer = compose(
+    applyMiddleware(thunk),
+    global.reduxNativeDevTools ?
+      global.reduxNativeDevTools(/*options*/) :
+      noop => noop
+  );
+
   const store = createStore(
-    reducer,
-    applyMiddleware(thunk)
+    reducer, {}, enhancer
   );
   syncOffline(store);
+
+  if (global.reduxNativeDevTools) {
+    global.reduxNativeDevTools.updateStore(store);
+  }
 
   return store;
 }
