bbcd01a30b1321ca360a0540571010cfa6a2dcf5
Crazyglue <edbz2k2@gmail.com>
2016-10-16 22:20:58 -0500

    Update for android

--- index.android.js
+++ index.android.js
@@ -3,7 +3,7 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
-import App from './app/containers/App';
+import Root from './app/containers/Root';
 import configureStore from './app/stores/configure_store';
 
 const store = configureStore();
@@ -12,7 +12,7 @@ class Thrust extends Component {
   render() {
     return (
       <Provider store={store}>
-        <App />
+        <Root />
       </Provider>
     )
   }
