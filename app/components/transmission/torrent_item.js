24b7bfb3f362b6f7dc6514168aaa610b58166f75
Crazyglue <edbz2k2@gmail.com>
2016-10-16 23:32:44 -0500

    Split transmission torrent list item into its own component

---
+++ app/components/transmission/torrent_item.js
@@ -0,0 +1,52 @@
+/*jshint esversion: 6 */
+
+import React, { Component } from 'react';
+import { Container, Header, Content, Title, Button, Icon, Text, Card, CardItem, Thumbnail, InputGroup, Input, List, ListItem } from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid';
+import styles from '../../stylesheets/default';
+import * as Progress from 'react-native-progress';
+import isEqual from 'lodash/isEqual';
+
+export default class TorrentItem extends Component {
+  constructor(params) {
+    super(params);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("isEqual function");
+    console.log(isEqual(this.props, nextProps));
+    return !isEqual(this.props, nextProps);
+  }
+
+  render() {
+    console.log("TorrentItem Props:");
+    console.log(this.props);
+    torrent = this.props.data;
+
+    return(
+      <ListItem key={torrent.name}>
+        <Grid>
+          <Row>
+            <Title>{torrent.name}</Title>
+          </Row>
+          <Row style={{ justifyContent: 'space-around'}} >
+            <Progress.Bar height={30} width={300} progress={torrent.percentDone} />
+          </Row>
+          <Row style={{marginTop: 20, marginBottom: 20}}>
+            <Col size={1}>
+              <Text>D: {(torrent.rateDownload / 1000).toFixed(2)} KB/s</Text>
+              <Text>U: {(torrent.rateUpload / 1000).toFixed(2)} KB/s</Text>
+            </Col>
+            <Col size={1}>
+              <Text>{torrent.eta} seconds</Text>
+              <Text>{((torrent.sizeWhenDone - torrent.leftUntilDone) / 1000000).toFixed(2)} / {(torrent.sizeWhenDone / 1000000).toFixed(2)} MB</Text>
+            </Col>
+            <Col size={1}>
+              <Text>Status: {torrent.status}</Text>
+            </Col>
+          </Row>
+        </Grid>
+      </ListItem>
+    )
+  }
+}
--- app/components/transmission/transmission_page.js
+++ app/components/transmission/transmission_page.js
@@ -9,6 +9,7 @@ import * as actionCreators from '../../actions/transmission';
 import { connect } from 'react-redux';
 import * as Progress from 'react-native-progress';
 import timer from 'react-native-timer';
+import TorrentItem from './torrent_item';
 
 class TransmissionPage extends Component {
   constructor(params) {
@@ -30,39 +31,14 @@ class TransmissionPage extends Component {
     this.props.getStats();
   }
 
+  renderTorrent(data) {
+    data.status = this.props.transmission.api.parseTorrentStatus(data.status);
+    return (<TorrentItem data={data} />)
+  }
+
   render() {
     console.log("TransmissionPage Props:");
     console.log(this.props);
-
-    rows = [];
-
-    this.props.transmission.displayTorrents.forEach((torrent) => {
-      rows.push(
-        <ListItem key={torrent.name}>
-          <Grid>
-            <Row>
-              <Title>{torrent.name}</Title>
-            </Row>
-            <Row style={{ justifyContent: 'space-around'}} >
-              <Progress.Bar height={30} width={300} progress={torrent.percentDone} />
-            </Row>
-            <Row style={{marginTop: 20, marginBottom: 20}}>
-              <Col size={1}>
-                <Text>D: {(torrent.rateDownload / 1000).toFixed(2)} KB/s</Text>
-                <Text>U: {(torrent.rateUpload / 1000).toFixed(2)} KB/s</Text>
-              </Col>
-              <Col size={1}>
-                <Text>{torrent.eta}</Text>
-              </Col>
-              <Col size={1}>
-                <Text>Status: {this.props.transmission.api.parseTorrentStatus(torrent.status)}</Text>
-              </Col>
-            </Row>
-          </Grid>
-        </ListItem>
-      )
-    });
-
     return(
       <Container>
         <Header>
@@ -77,9 +53,10 @@ class TransmissionPage extends Component {
         <Content>
           <Button onPress={this.getStats.bind(this)} block primary>Get Stats</Button>
           <Button onPress={() => this.props.getTorrentInfo([])} block warning>Get Torrent Info</Button>
-          <List>
-            {rows}
-          </List>
+          <List
+            dataArray={this.props.transmission.displayTorrents}
+            renderRow={this.renderTorrent.bind(this)}
+            />
 
         </Content>
       </Container>
