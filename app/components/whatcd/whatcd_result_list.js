5bfa731bf414b206a74c5dbdc776ddb5ba194c7b
Crazyglue <edbz2k2@gmail.com>
2016-10-28 09:33:22 -0500

    Remove unneeded imports in whatcd result list

--- app/components/whatcd/whatcd_result_list.js
+++ app/components/whatcd/whatcd_result_list.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import * as actionCreators from '../../actions/whatcd';
 import { connect } from 'react-redux';
 import WhatCDResult from './whatcd_result';
-import { ListView, ScrollView } from 'react-native';
-import { Container, Header, Content, Title, Button, Icon, List, ListItem, Text, Card, CardItem, Thumbnail, InputGroup, Input } from 'native-base';
-import { Col, Row, Grid } from 'react-native-easy-grid';
+import { ScrollView } from 'react-native';
+import { Content } from 'native-base';
 import sortBy from 'lodash/sortBy';
 
 class WhatCDResultList extends Component {
