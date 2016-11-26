049a3e19f7e4323e60356692febad931d3641e6a
Crazyglue <edbz2k2@gmail.com>
2016-11-26 16:54:16 -0600

    Style and use poster in show page

--- app/components/sickrage/show.js
+++ app/components/sickrage/show.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
+import { Image, StyleSheet, View, Dimensions } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { Container, Header, Content, Title, Button, Icon, Text, Thumbnail, InputGroup, Input, Spinner, List, ListItem, Card, CardItem } from 'native-base';
 import { Col, Row, Grid } from 'react-native-easy-grid';
@@ -9,34 +9,68 @@ export default class Show extends Component {
   constructor(params) {
     super(params);
 
-    this.state = {};
+    this.state = {
+      // height: ###, width: ###, scale: #
+      windowDimensions: Dimensions.get('window')
+    };
+  }
+
+  _renderRow(season, index, rowID, highlightRow) {
+    // console.log("renderRow() season", season)
+    // console.log("renderRow() index", index);
+    // console.log("renderRow() rowID", rowID);
+    // console.log("renderRow() highlightRow", highlightRow);
+    if (rowID != 0) {
+      return(
+        <ListItem>
+          <Text>Season {rowID}</Text>
+        </ListItem>
+      )
+    }
+    else {
+      return(
+        <ListItem style={ { backgroundColor: "orange" } }>
+          <Text>Extras!</Text>
+        </ListItem>
+      )
+    }
   }
 
   render() {
     console.log("Show Props:");
     console.log(this.props);
-
-    seasons = _.map(this.props.show.seasons, (season, index) => {
-      return <Text>Season {index}</Text>
-    })
-
-    console.log("seasons:", seasons);
+    // console.log("seasons:", this.props.show.seasons);
 
     return (
       <Container>
         <Header>
+          <Title>{this.props.show.show_name}</Title>
           <Button onPress={Actions.pop} transparent>
             <Icon name="ios-arrow-back" />
           </Button>
         </Header>
         <Content>
-          <Text>Show</Text>
-          <Image source={{uri: this.props.show.image}} style={{ width: 758/2.2, height: 140/2.2 }} />
-          <Text>{this.props.show.show_name}</Text>
-          <Text>{this.props.show.status}</Text>
-          {seasons}
+          <View style={styles.container}>
+            <Image source={{uri: this.props.show.poster}} style={styles.poster} />
+            <List
+              style={{ width: this.state.windowDimensions.width }}
+              dataArray={this.props.show.seasons}
+              renderRow={this._renderRow}
+              />
+            <Text>{this.props.show.status}</Text>
+          </View>
         </Content>
       </Container>
     )
   }
 }
+
+var styles = StyleSheet.create({
+  container: {
+    alignItems: 'center'
+  },
+  poster: {
+    width: 411 / 1.5,
+    height: 605 / 1.5
+  },
+})
