1e6c5b41fde1508501b3846f4a678bad15b0cf5f
Crazyglue <edbz2k2@gmail.com>
2016-11-26 16:01:27 -0600

    Fix android react-native-fetch-blob error

--- android/app/src/main/java/com/thrust/MainApplication.java
+++ android/app/src/main/java/com/thrust/MainApplication.java
@@ -3,6 +3,7 @@ package com.thrust;
 import android.app.Application;
 import android.util.Log;
 
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -23,7 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNFetchBlobPackage()
       );
     }
   };
